b3aa43e991a72939ec2e2f218b3dd9bd8003e254
Thanh Nguyen <tienthanh8490@gmail.com>
2016-07-14 13:46:02 +0800

    permutation ver 2

--- misc/permutations/permutations.py
+++ misc/permutations/permutations.py
@@ -1,7 +1,23 @@
-def permutations(head, tail=''):
-    if len(head) == 0: print tail
+def permutations1(head, tail=''):
+    if len(head) == 0: print 'result->',tail
     else:
         for i in range(len(head)):
-            permutations(head[0:i] + head[i+1:], tail+head[i])
+            print head[0:i],head[i+1:],tail+head[i] # tracing recursion tree
+            permutations1(head[0:i] + head[i+1:], tail+head[i])
 
-print permutations('abcd')
+def permutations2(s):
+   if len(s) == 1:
+     return [s]
+
+   result = [] # resulting list
+   for char in s:
+     remaining_elements = [x for x in s if x != char]
+     remaining_permutations = permutations2(remaining_elements) # permutations of sublist
+
+     for permutation in remaining_permutations:
+       result.append([char] + permutation)
+
+   return result
+
+permutations1('abcd')
+print permutations2('abcd')
