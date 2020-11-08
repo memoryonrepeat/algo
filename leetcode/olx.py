54057f8056c4e0f8848cacf85204cd212e3437bd
Thanh Nguyen <thanh.nguyen@frontiercargroup.com>
2020-11-08 20:44:23 +0100

    olx + 3 sums

--- leetcode/olx.py
+++ leetcode/olx.py
@@ -1,1 +1,58 @@
 #https://app.codility.com/cert/view/certYC759C-PUAA3MRV56P8KJXV/details/
+
+# Task description
+# Rick is really fond of fruit juices, but he is bored of their traditional flavours. Therefore, he has decided to mix as many of them as possible to obtain something entirely new as a result.
+
+# He has N glasses, numbered from 0 to N-1, each containing a different kind of juice. The J-th glass has capacity[J] units of capacity and contains juice[J] units of juice. In each glass there is at least one unit of juice.
+
+# Rick want to create a multivitamin mix in one of the glasses. He is going to do it by pouring juice from several other glasses into the chosen one. Each of the used glasses must be empty at the end (all of the juice from each glass has to be poured out).
+
+# What is the maximum number of flavours that Rick can mix?
+
+# Write a function:
+
+# def solution(juice, capacity)
+
+# that, given arrays juice and capacity, both of size N, returns the maximum number of flavours that Rick can mix in a single glass.
+
+# Examples:
+
+# 1. Given juice = [10, 2, 1, 1] and capacity = [10, 3, 2, 2], your function should return 2. Rick can pour juice from the 3rd glass into the 2nd one.
+
+# 2. Given juice = [1, 2, 3, 4] and capacity = [3, 6, 4, 4], your function should return 3. Rick can pour juice from the 0th and 2nd glasses into the 1st one.
+
+# 3. Given juice = [2, 3] and capacity = [3, 4], your function should return 1. No matter which glass he chooses, Rick cannot pour juice from the other one into it. The maximum number of flavours in the chosen glass is 1.
+
+# 4. Given juice = [1, 1, 5] and capacity = [6, 5, 8], your function should return 3. Rick can mix all juices in the 2nd glass.
+
+# Write an efficient algorithm for the following assumptions:
+
+# N is an integer within the range [2..100,000];
+# each element of arrays juice, capacity is an integer within the range [1..1,000,000,000];
+# arrays juice and capacity have the same length, equal to N;
+# for each J juice[J] ≤ capacity[J].
+
+def solution(juice, capacity):
+    # write your code in Python 3.6
+    remaining_capacity = [c - juice[i] for i,c in enumerate(capacity)]
+    sorted_juice = sorted(juice) # low to high
+    result = 1
+    # print(remaining_capacity)
+    for current,rc in enumerate(remaining_capacity):
+        skipped_own = False
+        local_result = 1
+        for sj in sorted_juice:
+            if sj == juice[current]:
+                if skipped_own == False:
+                    skipped_own = True
+                    continue
+            rc -= sj
+            if rc >= 0:
+                local_result += 1
+                result = max(result, local_result)
+            else:
+                break
+    return result
+                    
+            
+
---
+++ leetcode/threesum.py
@@ -0,0 +1,25 @@
+# https://leetcode.com/problems/3sum/
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        total = 0
+        table = {}
+        result = set()
+        for num in nums:
+            if num not in table:
+                table[num] = 1
+            else:
+                table[num] += 1
+        for key1 in table:
+            table[key1] -= 1
+            for key2 in table:
+                if table[key2] == 0:
+                    continue
+                table[key2] -= 1
+                key3 = total - key1 - key2
+                if key3 in table and table[key3] > 0:
+                    new = tuple(sorted([key1, key2, total - key1 - key2]))
+                    result.add(new)
+                table[key2] += 1
+            table[key1] += 1
+        return result
+            
