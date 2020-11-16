796d1d78936f5b7518db8e3bb95f8aff650f3089
Thanh Nguyen <tienthanh8490@gmail.com>
2020-11-17 00:00:43 +0100

    reaching points

---
+++ leetcode/reachingpoints.py
@@ -0,0 +1,17 @@
+# https://leetcode.com/problems/reaching-points/
+class Solution(object):
+    def reachingPoints(self, sx, sy, tx, ty):
+        while tx >= sx and ty >= sy:
+            if tx == ty:
+                break
+            elif tx > ty:
+                if ty > sy:
+                    tx %= ty
+                elif ty == sy:
+                    return (tx - sx) % sy == 0
+            else:
+                if tx > sx:
+                    ty %= tx
+                elif tx == sx:
+                    return (ty - sy) % sx == 0
+        return tx == sx and ty == sy
