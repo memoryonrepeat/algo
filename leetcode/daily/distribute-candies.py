0fbe402789bb798be191e9803d7097942b7884bd
Thanh Nguyen <tienthanh8490@gmail.com>
2021-03-01 15:30:45 +0100

    distribute candies

---
+++ leetcode/daily/distribute-candies.py
@@ -0,0 +1,5 @@
+# https://leetcode.com/problems/distribute-candies/
+
+class Solution:
+    def distributeCandies(self, candyType: List[int]) -> int:
+        return int(min(len(candyType)/2, len(set(candyType))))
