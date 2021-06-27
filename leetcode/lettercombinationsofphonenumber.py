3ac58ff02a5a1abde7c06be720906f207f08672f
Thanh Nguyen <tienthanh8490@gmail.com>
2021-06-27 13:10:41 +0200

    letter combinations using recursion

--- leetcode/lettercombinationsofphonenumber.py
+++ leetcode/lettercombinationsofphonenumber.py
@@ -19,4 +19,27 @@ class Solution:
             else:
                 result = [x+y for x in result for y in mapping[digit]]
         return result
-        
+
+# Alternative way using recursion        
+class Solution2:
+    def letterCombinations(self, digits: str) -> List[str]:
+        result = []
+        mapping = {
+            "2": ["a","b","c"],
+            "3": ["d","e","f"],
+            "4": ["g","h","i"],
+            "5": ["j","k","l"],
+            "6": ["m","n","o"],
+            "7": ["p","q","r","s"],
+            "8": ["t","u","v"],
+            "9": ["w","x","y","z"]
+        }
+        if len(digits) == 0:
+            return ''
+        if len(digits) == 1:
+            return mapping[digits[0]]
+        for c in mapping[digits[0]]:
+            sub = self.letterCombinations(digits[1:])
+            for s in sub:
+                result.append(c+s)
+        return result
