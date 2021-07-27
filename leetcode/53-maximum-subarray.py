6a08a4bad31a6ca3b032b2c433aaeb16df6f7528
Thanh Nguyen <tienthanh8490@gmail.com>
2021-07-27 13:10:24 +0200

    maximum subarray

---
+++ leetcode/53-maximum-subarray.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/maximum-subarray/
+
+class Solution:
+    def maxSubArray(self, a): 
+        size = len(a)
+        max_so_far =a[0] 
+        curr_max = a[0] 
+
+        for i in range(1,size): 
+            curr_max = max(a[i], curr_max + a[i]) 
+            max_so_far = max(max_so_far,curr_max) 
+
+        return max_so_far 
