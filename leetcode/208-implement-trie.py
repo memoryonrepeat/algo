9c1046ae563a8ce321bd8a672fc7e9513b60b2be
Thanh Nguyen <tienthanh8490@gmail.com>
2021-10-13 12:01:42 +0200

    trie solution 2

--- leetcode/208-implement-trie.py
+++ leetcode/208-implement-trie.py
@@ -36,6 +36,44 @@ class Trie:
             current = current.child[char]
         return True
 
+# Alternative solution, no need TrieNode class
+class __Trie:
+
+    def __init__(self):
+        self.root = {}
+
+    def insert(self, word: str) -> None:
+        current = self.root
+        for char in word:
+            if char not in current:
+                current[char] = {}
+            current = current[char]
+        current['end'] = True
+
+    def search(self, word: str) -> bool:
+        current = self.root
+        for char in word:
+            if char not in current:
+                return False
+            current = current[char]
+        if 'end' in current:
+            return True
+        return False
+
+    def startsWith(self, prefix: str) -> bool:
+        current = self.root
+        for char in prefix:
+            if char not in current:
+                return False
+            current = current[char]
+        return True
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
+
 # Your Trie object will be instantiated and called as such:
 # obj = Trie()
 # obj.insert(word)
