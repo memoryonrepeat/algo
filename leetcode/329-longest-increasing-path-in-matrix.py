2253a94798a2f4eb06b3884d76fb639c9130391a
Thanh Nguyen <tienthanh8490@gmail.com>
2024-03-01 22:25:03 +0000

    longest increasing path in matrix

--- leetcode/329-longest-increasing-path-in-matrix.py
+++ leetcode/329-longest-increasing-path-in-matrix.py
@@ -27,3 +27,34 @@ class Solution:
         dp_table[i][j] = res
         return res
         
+# Redo 1/3/2024
+# Using same backtrack technique as LC79, adding memoization to optimize
+class Solution:
+    def __init__(self):
+        self.ans = 1
+    
+    def longestIncreasingPath(self, matrix: List[List[int]]) -> int:
+        m = len(matrix)
+        n = len(matrix[0])
+        directions = [(-1,0),(1,0),(0,-1),(0,1)]
+        seen = {}
+                
+        def backtrack(i,j):               
+            if (i,j) in seen:
+                return seen[(i,j)]
+            
+            bestMove = 1
+            
+            for dx,dy in directions:
+                if 0<=i+dx<m and 0<=j+dy<n and matrix[i][j] < matrix[i+dx][j+dy]:
+                    bestMove = max(bestMove, 1 + backtrack(i+dx,j+dy))
+                    
+            seen[(i,j)] = bestMove
+                    
+            return seen[(i,j)]
+            
+        for i in range(m):
+            for j in range(n):
+                self.ans = max(self.ans, backtrack(i,j))
+        
+        return self.ans        
