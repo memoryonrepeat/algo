696cc3b1a289cb6e64640cd47dadfdd0857cadb1
Thanh Nguyen <tienthanh8490@gmail.com>
2024-03-03 17:20:08 +0000

    3sum

--- leetcode/threesum.py
+++ leetcode/threesum.py
@@ -22,4 +22,32 @@ class Solution:
                 table[key2] += 1
             table[key1] += 1
         return result
+        
+# Redo 3/3/24
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        ans = []
+        seen = set()
+        for i, num in enumerate(nums):
+            if num in seen:
+                continue
+            seen.add(num)
+            candidates = self.twoSum(nums[i+1:], -num)
+            if candidates:
+                ans += list(map(lambda c: [num,c[0],c[1]], candidates))
+        return ans
+
+    def twoSum(self, nums, target):
+        if not nums:
+            return []
+        seen = {}
+        ans = set()
+        for i,num in enumerate(nums):
+            if num not in seen:
+                seen[num] = i
+            if target - num in seen and i != seen[target-num] :
+                ans.add((num, target-num))
+        return ans
+        
             
