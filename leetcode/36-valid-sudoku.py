49831360f75bb752c76241f6fc806392bf270ede
Thanh Nguyen <tienthanh8490@gmail.com>
2024-03-05 15:03:01 +0000

    valid sudoku improved

--- leetcode/36-valid-sudoku.py
+++ leetcode/36-valid-sudoku.py
@@ -1,3 +1,5 @@
+from functools import reduce
+
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         rows = [[(i,j) for j in range(9)] for i in range(9)]
@@ -15,13 +17,9 @@ class Solution:
                 ] for x in [0,3,6] for y in [0,3,6]]
 
         def isValidComponent(c):
-            seen = set()
-            for x,y in c:
-                if board[x][y] != ".":
-                    if board[x][y] not in seen:
-                        seen.add(board[x][y])
-                    else:
-                        return False
-            return True
+            nonEmptyCells = list(filter(lambda cell: board[cell[0]][cell[1]] != ".", c))
+            nonEmptyValues = list(map(lambda cell: board[cell[0]][cell[1]] , nonEmptyCells))
 
-        return all([isValidComponent(row) for row in rows]) and all([isValidComponent(col) for col in cols]) and all([isValidComponent(box) for box in boxes])
+            return len(nonEmptyValues) == len(set(nonEmptyValues))
+
+        return all(map(isValidComponent,rows+cols+boxes))
