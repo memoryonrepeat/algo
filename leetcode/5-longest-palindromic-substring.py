e744dfbf5e856b231b27999f81921bb86051ce48
Thanh Nguyen <tienthanh8490@gmail.com>
2024-03-05 22:40:20 +0000

    longest palindromic substring

--- leetcode/5-longest-palindromic-substring.py
+++ leetcode/5-longest-palindromic-substring.py
@@ -19,3 +19,26 @@ class Solution:
                     end = j
                     start = i
         return s[start:end+1]
+
+# Redo 5/3/24
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        n = len(s)
+        dp = [[False for _ in range(n)] for _ in range(n)]
+        ans = [0,0]
+
+        for i in range(n):
+            dp[i][i] = True
+
+        for i in range(n-1):
+            dp[i][i+1] = (s[i] == s[i+1])
+            ans = [i,i+1] if dp[i][i+1] else ans
+
+        for diff in range(2,n):
+            for i in range(n-diff):
+                j = i+diff
+                if s[i] == s[j] and dp[i+1][j-1]:
+                    dp[i][j] = True
+                    ans = [i,j]
+
+        return s[ans[0] : ans[1]+1]
