57e14d6f58e21cf5cdf48eaebedd980ef03249b0
Thanh Nguyen <tienthanh8490@gmail.com>
2024-07-28 21:54:44 +0100

    399 refactored

--- leetcode/399-evaluate-division.go
+++ leetcode/399-evaluate-division.go
@@ -1,3 +1,5 @@
+// v1
+
 import (
     "slices"
 )
@@ -56,3 +58,59 @@ func calcEquation(equations [][]string, values []float64, queries [][]string) []
 
     return result
 }
+
+// v2 - refactored
+
+import (
+    "fmt"
+)
+
+type Edge struct {
+    node     string
+    division float64
+}
+
+func calcEquation(equations [][]string, values []float64, queries [][]string) []float64 {
+    adjMatrix := make(map[string][]Edge)
+
+    // Build the adjacency matrix
+    for i, equation := range equations {
+        v := values[i]
+        adjMatrix[equation[0]] = append(adjMatrix[equation[0]], Edge{equation[1], v})
+        adjMatrix[equation[1]] = append(adjMatrix[equation[1]], Edge{equation[0], 1 / v})
+        adjMatrix[equation[0]] = append(adjMatrix[equation[0]], Edge{equation[0], 1})
+    }
+
+    // Depth-first search function
+    var dfs func(start, end string, visited map[string]bool, total float64) float64
+    dfs = func(start, end string, visited map[string]bool, total float64) float64 {
+        visited[start] = true
+        neighbors := adjMatrix[start]
+        if len(neighbors) == 0 {
+            return -1
+        }
+        for _, edge := range neighbors {
+            if edge.node == end {
+                return total * edge.division
+            }
+            if visited[edge.node] {
+                continue
+            }
+            result := dfs(edge.node, end, visited, total*edge.division)
+            if result > -1 {
+                return result
+            }
+        }
+        return -1
+    }
+
+    // Process each query
+    result := make([]float64, len(queries))
+    for i, query := range queries {
+        visited := make(map[string]bool)
+        result[i] = dfs(query[0], query[1], visited, 1)
+    }
+
+    return result
+}
+
