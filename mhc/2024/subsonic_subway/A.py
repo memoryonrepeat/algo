7ae9ebc3ba6ab07fab5103d371d6e9af42691e47
Thanh Nguyen <tienthanh8490@gmail.com>
2024-11-21 20:39:44 +0000

    solve by merging intervals

--- mhc/2024/subsonic_subway/A.py
+++ mhc/2024/subsonic_subway/A.py
@@ -6,7 +6,7 @@ T = int(f.readline())
 result = []
 
 # Translate windows to pairs of min / max velocity required for each stop
-# Then merge those intervals, output -1 if impossible to converge to 1 interval
+# Solve by comparing min / max required velocity only
 def solve(windows):
 	minVelocity = 1/windows[0][1]
 	maxVelocity = 1/windows[0][0] if windows[0][0] > 0 else 999999999999
@@ -23,6 +23,33 @@ def solve(windows):
 		return
 	result.append(minVelocity)
 
+# Solve by merging intervals
+def __solve(windows):
+	intervals = []
+	for i in range(len(windows)):
+		if windows[i][0] == 0:
+			windows[i][0] = 0.000000001
+		intervals.append([(i+1)/windows[i][1], (i+1)/windows[i][0]])
+
+	intervals.sort(key = lambda v: v[0])
+
+	current = None
+	impossible = False
+
+	for low,high in intervals:
+		if not current:
+			current = [low,high]
+			continue
+		if low > current[1]:
+			impossible = True
+			break
+		current[0] = max(current[0], low)
+		current[1] = min(current[1], high)
+
+	if impossible:
+		result.append(-1)
+		return
+	result.append(current[0])
 
 for _ in range(T):
 	n = int(f.readline())
