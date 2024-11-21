b60d6414f9e5271824134da9a3fa0296679d08e0
Thanh Nguyen <tienthanh8490@gmail.com>
2024-11-21 16:34:09 +0000

    compare min / max only

--- mhc/2024/subsonic_subway/A.py
+++ mhc/2024/subsonic_subway/A.py
@@ -8,31 +8,20 @@ result = []
 # Translate windows to pairs of min / max velocity required for each stop
 # Then merge those intervals, output -1 if impossible to converge to 1 interval
 def solve(windows):
-	intervals = []
-	for i in range(len(windows)):
+	minVelocity = 1/windows[0][1]
+	maxVelocity = 1/windows[0][0] if windows[0][0] > 0 else 999999999999
+	for i in range(1,len(windows)):
+		minVelocity = max(minVelocity, (i+1)/windows[i][1])
 		if windows[i][0] == 0:
-			windows[i][0] = 0.000000001
-		intervals.append([(i+1)/windows[i][1], (i+1)/windows[i][0]])
+			maxVelocity = min(maxVelocity, 999999999999)
+		else:
+			maxVelocity = min(maxVelocity, (i+1)/windows[i][0])
+	# print(windows, minVelocity, maxVelocity)
 
-	intervals.sort(key = lambda v: v[0])
-
-	current = None
-	impossible = False
-
-	for low,high in intervals:
-		if not current:
-			current = [low,high]
-			continue
-		if low > current[1]:
-			impossible = True
-			break
-		current[0] = max(current[0], low)
-		current[1] = max(current[1], high)
-
-	if impossible:
+	if minVelocity > maxVelocity:
 		result.append(-1)
 		return
-	result.append(current[0])
+	result.append(minVelocity)
 
 
 for _ in range(T):
@@ -46,6 +35,6 @@ for _ in range(T):
 
 f.close()
 fw = open("result.txt", "w")
-to_write = "\n".join(["Case #{i}: {r:.10f}".format(i=i+1,r=result[i]) for i in range(len(result))])
+to_write = "\n".join(["Case #{i}: {r:.7f}".format(i=i+1,r=result[i]) for i in range(len(result))])
 fw.write(to_write)
 fw.close()
